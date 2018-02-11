a504482c7b0db27cd8f258a2ff6566fcfaf96afe
Maki Yamamoto <maki.yamamoto@wework.com>
2018-02-11 00:21:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,13 @@
 
 def display_rainbow(colors)
   puts #{colors(0)}
-  
+  puts #{colors(1)}
+  puts #{colors(2)}
+  puts #{colors(3)}
+  puts #{colors(4)}
+  puts #{colors(5)}
+  puts #{colors(6)}
+  puts #{colors(7)}
+end
+
+display_rainbow(colors)
